49e7628eb12657cda7207486f91436255fa0fbf4
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-12-21 21:49:23 +0900

    Drop workaround for python < 3.10
    Since Python 3.10 methods inherits the original attributes even when
    these are decorated. So we can safely assume that the common attributes
    such as __qualname__ should exist.
    
    Change-Id: I4490c1186222c3cfdb89e07f70711d2263885dae
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- automaton/_utils.py
+++ automaton/_utils.py
@@ -15,24 +15,11 @@
 def get_callback_name(cb):
     """Tries to get a callbacks fully-qualified name.
 
-    If no name can be produced ``repr(cb)`` is called and returned.
     """
-    segments = []
-    try:
-        segments.append(cb.__qualname__)
-    except AttributeError:
-        try:
-            segments.append(cb.__name__)
-        except AttributeError:
-            pass
+    segments = [cb.__qualname__]
 
-    if not segments:
-        return repr(cb)
-    else:
-        try:
-            # When running under sphinx it appears this can be none?
-            if cb.__module__:
-                segments.insert(0, cb.__module__)
-        except AttributeError:
-            pass
-        return ".".join(segments)
+    # When running under sphinx it appears this can be none?
+    if cb.__module__:
+        segments.insert(0, cb.__module__)
+
+    return ".".join(segments)
